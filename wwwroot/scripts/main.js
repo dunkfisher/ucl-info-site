bb7f6d81ff27c3aa5e1f9f0340c7ac7e8873e376
dunkfisher <dunc1981@gmail.com>
2023-10-06 00:50:36 +0100

    Animate burger menu.

--- wwwroot/scripts/main.js
+++ wwwroot/scripts/main.js
@@ -1,4 +1,11 @@
 $(function () {
+    // Burger menu
+    $("#burger-trigger").on("click", function () {
+        $(this).toggleClass("close");
+        $("#burger-icon").toggleClass("close");
+    });
+
+    // Collapsible sections
     $(".collapse-section-header").on("click", function() {        
         var sectionBody = $(this).next(".collapse-section-body");   
         sectionBody.slideToggle();
